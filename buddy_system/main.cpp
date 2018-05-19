9050ffd16fa4abc1bc8271c8e15412ffa048d9c1
以明 <lumindoec@yimingdemacbook-air.local>
2018-05-19 14:36:58 +0800

    Initial Commit

---
+++ buddy_system/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  buddy_system
+//
+//  Created by 以明 on 2018/5/19.
+//  Copyright © 2018年 以明. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
